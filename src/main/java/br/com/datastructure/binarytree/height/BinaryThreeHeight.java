9199d66c35c76ade71d6c026de24be40ef906982
joaocarlos1994 <joao.carl98@gmail.com>
2020-02-22 11:46:25 -0300

    First Commit

---
+++ src/main/java/br/com/datastructure/binarytree/height/BinaryThreeHeight.java
@@ -0,0 +1,4 @@
+package br.com.datastructure.binarytree.height;
+
+public class BinaryThreeHeight {
+}
---
+++ src/main/java/br/com/datastructure/binarytree/ordertraversal/BinaryThreeOrderTraversal.java
@@ -0,0 +1,4 @@
+package br.com.datastructure.binarytree.ordertraversal;
+
+public class BinaryThreeOrderTraversal {
+}
